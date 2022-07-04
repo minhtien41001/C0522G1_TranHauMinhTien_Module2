df57b48648ad115ce91014edc6d46a5f4fa2589b
unknown <you@example.com>
2022-07-05 00:39:13 +0700

    1234

--- src/ss2_vonglapJava/bai_tap/DisplayPrimesLessThan100.java
+++ src/ss2_vonglapJava/bai_tap/DisplayPrimesLessThan100.java
@@ -1,2 +1,19 @@
-package ss2_vonglapJava.bai_tap;public class DisplayPrimesLessThan100 {
+package ss2_vonglapJava.bai_tap;
+
+public class DisplayPrimesLessThan100 {
+    public static void main(String[] args) {
+        int number = 2;
+        while (number < 100) {
+            int divisor = 0;
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    divisor++;
+                }
+            }
+            if (divisor == 2) {
+                System.out.println(number);
+            }
+            number ++;
+        }
+    }
 }
--- src/ss2_vonglapJava/bai_tap/PrimeNumberDisplay.java
+++ src/ss2_vonglapJava/bai_tap/PrimeNumberDisplay.java
@@ -1,2 +1,20 @@
-package ss2_vonglapJava.bai_tap;public class PrimeNumberDisplay {
+package ss2_vonglapJava.bai_tap;
+
+public class PrimeNumberDisplay {
+    public static void main(String[] args) {
+        int number = 2;
+        int count = 0;
+        while (count < 20){
+            int divisor = 0;
+            for (int i = 1; i<= number;i++) {
+                if (number % i == 0) {
+                    divisor++;
+                }
+            }
+            if (divisor == 2){
+                System.out.println(number);
+                count++;
+            } number++;
+        }
+    }
 }
