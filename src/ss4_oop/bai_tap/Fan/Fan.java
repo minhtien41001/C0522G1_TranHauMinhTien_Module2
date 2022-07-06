15fb2df1a1cc2d3a0a3b117c75195faa986d2f58
unknown <you@example.com>
2022-07-07 06:24:00 +0700

    1234

--- src/ss4_oop/bai_tap/Fan/Fan.java
+++ src/ss4_oop/bai_tap/Fan/Fan.java
@@ -1,4 +1,62 @@
 package ss4_oop.bai_tap.Fan;
 
 public class Fan {
+    private final int SLOW = 1;
+    private final int MEDIUM = 2;
+    private final int FAST = 3;
+
+    private int speed;
+    private boolean on ;
+    private double radius;
+    private String color;
+
+    public Fan() {
+        this.speed = SLOW;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public String getSpeed() {
+        if(this.isOn()){
+            switch (this.speed){
+                case 1:
+                    return "fan is on SLOW";
+                case 2:
+                    return "fan is on MEDIUM";
+                case 3:
+                    return "fan is on FAST";
+            }
+        }
+        return "fan is off";
+
+    }
+
+    public boolean isOn() {
+        return this.on;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+
+    @Override
+    public String toString() {
+        if (!this.isOn()){
+            return this.getSpeed()+" radius = "+this.radius+" , color = "+this.color;
+        }
+        return this.getSpeed()+" radius = "+this.radius+" , color = "+this.color;
+    }
 }
--- src/ss4_oop/bai_tap/Fan/Main.java
+++ src/ss4_oop/bai_tap/Fan/Main.java
@@ -1,4 +1,12 @@
 package ss4_oop.bai_tap.Fan;
 
 public class Main {
+    public static void main(String[] args) {
+        Fan fan = new Fan();
+        System.out.println(fan);
+        Fan fan1 = new Fan(3,true,10,"red");
+        System.out.println(fan1);
+        Fan fan2 = new Fan(2,false,5,"blue");
+        System.out.println(fan2);
+    }
 }
